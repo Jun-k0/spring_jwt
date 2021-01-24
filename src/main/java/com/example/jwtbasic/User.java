9a90a1b055ff47cd900dc4a7356c0c55ca9a5161
jun-k0 <wnsrlsladlek@naver.com>
2021-01-24 23:46:04 +0900

    Initial commit

---
+++ src/main/java/com/example/jwtbasic/User.java
@@ -0,0 +1,25 @@
+package com.example.jwtbasic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class User {
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 300, nullable = false)
+    private String password;
+}
